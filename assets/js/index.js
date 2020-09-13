0d2b7a488b7bdd2e9a58c708da79951ad2b39434
seven <757665857@qq.com>
2020-09-13 09:17:46 +0800

    第5次修改

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,30 +22,30 @@ $(function() {
 
 // 获取用户的基本信息
 function getUserInfo() {
-    $.ajax({
-      method: 'GET',
-      url: '/my/userinfo',
-      success: function(res) {
-        if (res.status !== 0) {
-          return layui.layer.msg('获取用户信息失败！')
-        }
-        // 调用 renderAvatar 渲染用户的头像
-        renderAvatar(res.data)
-      },
-    //   不论成功还是失败，最终都会调用 complete 回调函数
-    //   complete: function(res) {
-    //     // console.log('执行了 complete 回调：')
-    //     // console.log(res)
-    //     // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-    //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-    //       // 1. 强制清空 token
-    //       localStorage.removeItem('token')
-    //       // 2. 强制跳转到登录页面
-    //       location.href = '/login.html'
-    //     }
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    success: function(res) {
+      if (res.status !== 0) {
+        return layui.layer.msg('获取用户信息失败！')
+      }
+      // 调用 renderAvatar 渲染用户的头像
+      renderAvatar(res.data)
+    }
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    // complete: function(res) {
+    //   // console.log('执行了 complete 回调：')
+    //   // console.log(res)
+    //   // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+    //     // 1. 强制清空 token
+    //     localStorage.removeItem('token')
+    //     // 2. 强制跳转到登录页面
+    //     location.href = '/login.html'
     //   }
-    })
-  }
+    // }
+  })
+}
 
 // 渲染用户的头像
 function renderAvatar(user) {
